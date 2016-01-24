71ab8f7c239f413c1da8194730e478d6cb330d08
Berendschot <Maarten@MacBook-Air-van-Maarten.local>
2016-01-24 17:22:37 +0100

    Initial commit

--- Source/UITableView+RoundedCorners.m
+++ Source/UITableView+RoundedCorners.m
@@ -18,19 +18,18 @@ static BOOL shouldAddSeperator = YES;
 - (void)displayCell:(UITableViewCell *)cell withRoundedCornersAtIndexPath:(NSIndexPath *)indexPath {
     NSInteger rows = [self numberOfRowsInSection:indexPath.section] - 1;
     CAShapeLayer *layer = [[CAShapeLayer alloc] init];
-    CGRect cellBounds = CGRectMake(0, 0, self.bounds.size.width, cell.bounds.size.height);
     
     BOOL addSeperator = NO;
     
     if (indexPath.row == 0 && indexPath.row == rows) {
-        [layer setPath:[UIBezierPath bezierPathWithRoundedRect:cellBounds cornerRadius:5.0f].CGPath];
+        [layer setPath:[UIBezierPath bezierPathWithRoundedRect:cell.bounds cornerRadius:5.0f].CGPath];
     } else if (indexPath.row == 0) {
-        [layer setPath:[UIBezierPath bezierPathWithRoundedRect:cellBounds
+        [layer setPath:[UIBezierPath bezierPathWithRoundedRect:cell.bounds
                                                byRoundingCorners:(UIRectCornerTopLeft|UIRectCornerTopRight)
                                                      cornerRadii:CGSizeMake(5.0f, 5.0f)].CGPath];
         addSeperator = YES;
     } else if (indexPath.row == rows) {
-        [layer setPath:[UIBezierPath bezierPathWithRoundedRect:cellBounds
+        [layer setPath:[UIBezierPath bezierPathWithRoundedRect:cell.bounds
                                                byRoundingCorners:(UIRectCornerBottomLeft|UIRectCornerBottomRight)
                                                      cornerRadii:CGSizeMake(5.0f, 5.0f)].CGPath];
     } else {
@@ -44,8 +43,9 @@ static BOOL shouldAddSeperator = YES;
     if (shouldAddSeperator == YES && addSeperator == YES) {
         CGFloat seperator = (1.0f / [UIScreen mainScreen].scale);
         CALayer *cellSeperator = [[CALayer alloc] init];
-        
-        [cellSeperator setFrame:CGRectMake(15.0f, cell.bounds.size.height - seperator, cell.bounds.size.width - 15.0f, seperator)];
+
+        [cellSeperator setFrame:CGRectMake(self.separatorInset.left, cell.bounds.size.height - seperator,
+                                           cell.bounds.size.width - 15.0f, seperator)];
         [cellSeperator setBackgroundColor:self.separatorColor.CGColor];
         [cell.layer addSublayer:cellSeperator];
     }
